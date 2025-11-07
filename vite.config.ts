401494178bf5b0915d4acb9620942667802ee603
Casey Taylor Sanders <caseytaylorsanders@gmail.com>
2025-11-07 14:29:31 -0500

    moved base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,12 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/have-a-shirt-made-preview/",
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: "/have-a-shirt-made-preview/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
