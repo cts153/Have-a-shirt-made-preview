a450cec008ffedae24ad9f1d84daa9c1cd1e547d
Casey Taylor Sanders <caseytaylorsanders@gmail.com>
2025-11-07 14:09:20 -0500

    added base path to Vite config for GH Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/have-a-shirt-made-preview/",
   server: {
     host: "::",
     port: 8080,
